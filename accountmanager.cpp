01dfc87689b32489a3df5da616e669951e2a25b9
jonathanm-12 <81660305+jonathanm-12@users.noreply.github.com>
2021-06-23 00:15:28 -0400

    add some file i/o functionality

--- accountmanager.cpp
+++ accountmanager.cpp
@@ -1,39 +1,96 @@
+// Warning: This code is very ugly and won't work. Just storing it here to log changes.
+
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using std::string;
 using std::cin;
 using std::cout;
+using std::fstream;
 
-class loginManager {
-
-private:
-    string password = "password";
-    string username = "user@email.com";
-    string attempted_username;
-    string attempted_password;
+class account_manager {
 
 public:
-    void login() {
+
+    string password,
+           username,
+           attempted_username,
+           attempted_password,
+           new_username,
+           new_password;
+
+    bool exists;
+
+};
+
+bool has_spaces(string username_input){
+    for (int i = 0; i < username_input.size(); i ++){
+
+    }
+
+}
+
+int main() {
+
+    account_manager account_manager;
+
+    string new_username = account_manager.username + '\r';
+    string new_attempted_user = account_manager.attempted_username + '\r';
+
+    std::ifstream inf("accountinfo.txt");
+
+
+    if (!inf) {
+        std::ofstream outf{"accountinfo.cpp"};
+    }
+
+    // if user clicks sign up button:
+
+    string line;
+    while(getline(inf, line))
+    {
+
+        // replace break with bool variable, it's weird
+        if (new_username == line) break;
+    
+    }
+
+    if (new_username == line) cout << "Sorry, that username already exists!" << "\n";
+    else {
+        cout << "Please create your password: ";
+        cin >> new_password;
+        cout << "Your account has been created!" << "\n";
+    }
+
+    // if user clicks login button:
+
+    // account_manager.login();
+
+
         cout << "Enter username: ";
         cin >> attempted_username;
-        if (attempted_username == username) {
+        while(getline(inf, line)){
+            if (new_attempted_user == line)
+                account_manager.exists = true;
+        }
+
+        if (account_manager.exists == true) {
             cout << "\n" << "Enter password: ";
             cin >> attempted_password;
+
+           while(getline(inf, line, '\n')){
+            if (new_attempted_user == line)
+                account_manager.exists = true;
+        }
+
             if (attempted_password == password) {
                 cout << "Successfully logged in" << "\n";
             }
         }
         else
-            cout << "Invalid username" << "\n";
+            cout << "Username doesn't exist in database" << "\n";
 
-    }
-};
 
-int main() {
-
-    loginManager loginManager;
-    loginManager.login();
-    
     return 0;
 }
