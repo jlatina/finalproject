e4555e126763ab3941908b20ff343a78e4a98235
VictoriaCarlsten <86012225+VictoriaCarlsten@users.noreply.github.com>
2021-06-29 20:56:42 +0200

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -1,5 +1,6 @@
 // Copyright Victoria Carlsten
 
+
 #include <chrono>
 #include <ctime>
 #include <iostream>
@@ -26,146 +27,227 @@ sf::Text choose;
 sf::Text option;
 // sf::Music music;
 
-void eventfunc(){
- sf::Event event;
-     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
+void playgame();
 
-      if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
-        window.close();
-}
-}
+class interface {
+ public:
 
-void windowfunc() {
+  void windowfunc() {
+    sf::Event event;
+    std::time_t time = std::time(NULL);
+    time_t newtime = time;  // change this
+    std::tm now = *std::localtime(&newtime);
 
-  std::time_t time = std::time(NULL);
-  time_t newtime = time;  // change this
-  std::tm now = *std::localtime(&newtime);
+    int hrang,
+        minang,
+        secang;
 
-  int hrang,
-      minang,
-      secang;
-
-  std::string shour = to_string(int(now.tm_hour)),
-              sminute = to_string(int(now.tm_min)),
-              sseconds = to_string(int(now.tm_sec));
+    std::string shour = to_string(int(now.tm_hour)),
+                sminute = to_string(int(now.tm_min)),
+                sseconds = to_string(int(now.tm_sec));
 
 // Create background image
-  sf::Image box_background;
-  box_background.create(10, 10, sf::Color::Yellow);
-  box_background.loadFromFile("background.png");
-  sf::Texture texture_background;
-  texture_background.loadFromImage(box_background);
-  sf::Sprite sprite_background;
-  sprite_background.setTexture(texture_background);
-  sprite_background.scale(sf::Vector2f(1.5, 1.4)); //1st wider 2nd higher
-  sprite_background.setPosition(1, 1);
+    sf::Image box_background;
+    box_background.create(10, 10, sf::Color::Yellow);
+    box_background.loadFromFile("background.png");
+    sf::Texture texture_background;
+    texture_background.loadFromImage(box_background);
+    sf::Sprite sprite_background;
+    sprite_background.setTexture(texture_background);
+    sprite_background.scale(sf::Vector2f(1.5, 1.4)); //1st wider 2nd higher
+    sprite_background.setPosition(1, 1);
 
 // create the first rectangle
-  sf::RectangleShape first(sf::Vector2f(30, 4));
-  first.setFillColor(sf::Color::White);
-  first.setPosition(410, 295);
-  first.setSize(sf::Vector2f(550, 150));
+    sf::RectangleShape first(sf::Vector2f(30, 4));
+    first.setFillColor(sf::Color::White);
+    first.setPosition(410, 295);
+    first.setSize(sf::Vector2f(550, 150));
 
 // create the second rectangle
-  sf::RectangleShape second(sf::Vector2f(30, 4));
-  second.setFillColor(sf::Color::Black);
-  second.setPosition(430, 315);
-  second.setSize(sf::Vector2f(510, 112));
+    sf::RectangleShape second(sf::Vector2f(30, 4));
+    second.setFillColor(sf::Color::Black);
+    second.setPosition(430, 315);
+    second.setSize(sf::Vector2f(510, 112));
 
 // create "Play Game"
-  sf::Font font;
-  font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
-  word_pg.setFont(font);
-  word_pg.setString("Play Game");
-  word_pg.setFillColor(sf::Color::White);
-  word_pg.setOutlineColor(sf::Color::Blue);
-  word_pg.setOutlineThickness(3);
-  word_pg.setPosition(430, 300);
-  word_pg.setCharacterSize(100);
+    sf::Font font;
+    font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
+    word_pg.setFont(font);
+    word_pg.setString("Play Game");
+    word_pg.setFillColor(sf::Color::White);
+    word_pg.setOutlineColor(sf::Color::Blue);
+    word_pg.setOutlineThickness(3);
+    word_pg.setPosition(430, 300);
+    word_pg.setCharacterSize(100);
 
 // create "Language"
-  // word_la.setFont(font);
-  // word_la.setString("Language Options");
-  // word_la.setFillColor(sf::Color::White);
-  // word_la.setPosition(1000, 80);
-  // word_la.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
+    // word_la.setFont(font);
+    // word_la.setString("Language Options");
+    // word_la.setFillColor(sf::Color::White);
+    // word_la.setPosition(1000, 80);
+    // word_la.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
 
 // Create "Username"
-  username.setFont(font);
-  username.setString("Username");
-  username.setFillColor(sf::Color::White);
-  username.setPosition(1090, 80);
-  username.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
+    username.setFont(font);
+    username.setString("Username");
+    username.setFillColor(sf::Color::White);
+    username.setPosition(1090, 80);
+    username.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
 
 // The clocktext
-  clocktext.setFont(font);
-  clocktext.setString(shour + ":" + sminute + ":" + sseconds);
-  clocktext.setFillColor(sf::Color::White);
-  clocktext.setCharacterSize(24);
-  clocktext.setPosition(610, 650);
-  clocktext.setCharacterSize(30);
-  // clocktext.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
+    clocktext.setFont(font);
+    clocktext.setString(shour + ":" + sminute + ":" + sseconds);
+    clocktext.setFillColor(sf::Color::White);
+    clocktext.setCharacterSize(24);
+    clocktext.setPosition(610, 650);
+    clocktext.setCharacterSize(30);
+    // clocktext.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
 
 // Create login/sign -up text
-  font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
-  login.setFont(font);
-  login.setString("Log in/Sign up");
-  login.setFillColor(sf::Color::White);
-  login.setPosition(100, 82);
-  login.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
+    font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
+    login.setFont(font);
+    login.setString("Log in/Sign up");
+    login.setFillColor(sf::Color::White);
+    login.setPosition(100, 82);
+    login.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
 
 // Music
-  // music.openFromFile("wii.wav");
-  // music.play();
+    // music.openFromFile("wii.wav");
+    // music.play();
 
-  sf::Clock tracktime;
-  double time_now = accuratetime();
+    sf::Clock tracktime;
+    double time_now = accuratetime();
 
-  window.clear();
-  // sf::Event event;
-  window.draw(sprite_background);
-  window.draw(first);
-  window.draw(second);
-  window.draw(word_pg);
-  // window.draw(word_la);
-  window.draw(username);
-  window.draw(login);
+    window.clear();
+    // sf::Event event;
+    window.draw(sprite_background);
+    window.draw(first);
+    window.draw(second);
+    window.draw(word_pg);
+    // window.draw(word_la);
+    window.draw(username);
+    window.draw(login);
 
-  time = std::time(NULL);
-  clocktext.setString(shour + ":" + sminute + ":" + sseconds);
-  sf::Time elapsedsec = tracktime.getElapsedTime();
+    time = std::time(NULL);
+    clocktext.setString(shour + ":" + sminute + ":" + sseconds);
+    sf::Time elapsedsec = tracktime.getElapsedTime();
 
-  if (now.tm_hour < 10)
-    shour = "0" + to_string(int(now.tm_hour));
-  else
-    shour = to_string(int(now.tm_hour));
+    if (now.tm_hour < 10)
+      shour = "0" + to_string(int(now.tm_hour));
+    else
+      shour = to_string(int(now.tm_hour));
 
-  if (now.tm_min < 10)
-    sminute = "0" + to_string(int(now.tm_min));
-  else
-    sminute = to_string(int(now.tm_min));
+    if (now.tm_min < 10)
+      sminute = "0" + to_string(int(now.tm_min));
+    else
+      sminute = to_string(int(now.tm_min));
 
-  if (now.tm_sec < 10)
-    sseconds = "0" + to_string(int(now.tm_sec));
-  else
-    sseconds = to_string(int(now.tm_sec));
+    if (now.tm_sec < 10)
+      sseconds = "0" + to_string(int(now.tm_sec));
+    else
+      sseconds = to_string(int(now.tm_sec));
 
-  clocktext.setString(shour + ":" + sminute + ":" + sseconds);
+    clocktext.setString(shour + ":" + sminute + ":" + sseconds);
 
-  hrang = (float(now.tm_hour) * 30) - 90;
-  minang = (float(now.tm_min) * 6) - 90;
-  secang = (float(now.tm_sec) * 6) - 90;
+    hrang = (float(now.tm_hour) * 30) - 90;
+    minang = (float(now.tm_min) * 6) - 90;
+    secang = (float(now.tm_sec) * 6) - 90;
 
-  window.draw(clocktext);
-  window.display();
+    window.draw(clocktext);
+
+    bool click;
+
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::MouseButtonPressed) {
+
+        auto location = word_pg.getPosition();
+        double dx = location.x - event.mouseButton.x;
+        double dy = location.y - event.mouseButton.y;
+
+        if (dx < 50 && dy < 50) {
+          std::cout << "Play button pressed" << "\n";
+          click = true;
+          playgame();
+          break;
+        }
+
+        auto location2 = back.getPosition();
+        double dx2 = location2.x - event.mouseButton.x;
+        double dy2 = location2.y - event.mouseButton.y;
+
+
+        // if (dx2 < 35 && dy2 < 35) {
+        //   std::cout << "Back button pressed" << "\n";
+        //   windowfunc();
+        //   break;
+        // }
+
+        auto location3 = login.getPosition();
+        double dx3 = location3.x - event.mouseButton.x;
+        double dy3 = location3.y - event.mouseButton.y;
+
+        if (dx2 < 35 && dy2 < 35) {
+          std::cout << "Login button pressed" << "\n";
+          // loginfunc();
+          break;
+        }
+
+      }
+    }
+
+    window.display();
+  }
+
+};
+
+
+void eventfunc() {
+  sf::Event event;
+  interface interface;
+  auto location2 = back.getPosition();
+  double dx2 = location2.x - event.mouseButton.x;
+  double dy2 = location2.y - event.mouseButton.y;
+
+
+  while (window.pollEvent(event)) {
+    if (event.type == sf::Event::Closed)
+      window.close();
+
+    if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
+      window.close();
+
+    if (event.type == sf::Event::MouseButtonPressed) {
+      // dx2 = 0;
+      // dy2 = 0;
+      dx2 = location2.x - event.mouseButton.x;
+      dy2 = location2.y - event.mouseButton.y;
+
+
+      if (dx2 > -200 && dy2 > -50) {
+        std::cout << "Back button pressed" << "\n";
+        // break;
+        // while (window.isOpen()) {
+
+        while(window.isOpen()) {
+          interface.windowfunc();
+          eventfunc();
+        }
+        // }
+        // window.clear();
+        // windowfunc();
+        // break;
+      }
+
+    }
+  }
 }
 
 void playgame() {
+
+  interface interace;
   while (window.isOpen()) {
 // should show game options
-    window.clear();
+
 
     eventfunc();
 
@@ -246,6 +328,8 @@ void playgame() {
     back.setPosition(60, 40);
     back.setCharacterSize(28);
 
+
+    window.clear();
     window.draw(sprite_gbackground);
     window.draw(choose);
     window.draw(back);
@@ -256,45 +340,20 @@ void playgame() {
     window.draw(sprite);
     window.draw(sprite1);
     window.display();
-  }
-}
-
-// void lanuagefunc() {
-//   while (window.isOpen()) {
-//     window.clear();
-//     sf::Font font;
-//     sf::RectangleShape rectangle1(sf::Vector2f(30, 4));
-//     rectangle1.setFillColor(sf::Color::Red);
-//     rectangle1.setPosition(100, 100);
-
-//     font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
-//     option.setFont(font);
-//     option.setString("Go Back");
-//     option.setFillColor(sf::Color::White);
-//     option.setPosition(60, 40);
-//     option.setCharacterSize(28);
-
-//     window.draw(option);
-//     window.draw(rectangle1);
-//     window.display();
-
-//     // Language Options
-//   }
-// }
-
-void loginfunc() {
-
-  while (window.isOpen()) {
-    window.clear();
+    // backfunc();
 
   }
 
 }
 
 int main() {
+
+  bool click;
+  interface interface;
+
+
   while (window.isOpen()) {
-    windowfunc();
-   
+    interface.windowfunc();
 
     sf::Event event;
 
@@ -305,58 +364,8 @@ int main() {
       if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
         window.close();
 
-      // opening a new window if mouse is pressed
-      if (event.type == sf::Event::MouseButtonPressed) {
-
-        auto location = word_pg.getPosition();
-        double dx = location.x - event.mouseButton.x;
-        double dy = location.y - event.mouseButton.y;
-
-        if (dx < 50 && dy < 50) {
-          playgame();
-          break;
-        }
-
-        auto location2 = back.getPosition();
-        double dx2 = location2.x - event.mouseButton.x;
-        double dy2 = location2.y - event.mouseButton.y;
-
-        if (dx2 < 35 && dy2 < 35) {
-          windowfunc();
-          break;
-        }
-
-        auto location3 = login.getPosition();
-        double dx3 = location3.x - event.mouseButton.x;
-        double dy3 = location3.y - event.mouseButton.y;
-
-        if (dx2 < 35 && dy2 < 35) {
-          loginfunc();
-          break;
-        }
-
-      }
-
-
-      // auto location1 = word_la.getPosition();
-      // double dx1 = location1.x - event.mouseButton.x;
-      // double dy1 = location1.y - event.mouseButton.y;
-
-      // if (dx1 < 50 && dy1 < 50) {
-      //   lanuagefunc();
-      //   break;
-      // }
-
     }
-      
-  }return 0;
 
+  }
+  return 0;
 }
-
-
-
-// To do;
-// Fix "back"
-// Be able to close the window even in the other functins. Created a seperate function, but maybe redundent?
-// Why is the "login" button not working? ;(
-
